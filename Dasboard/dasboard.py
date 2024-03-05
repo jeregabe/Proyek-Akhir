0b4f9dfbecebe54b2c492452d4d8e782eb500410
jeregabe <162022610+jeregabe@users.noreply.github.com>
2024-03-05 22:10:42 +0700

    Update dasboard.py

--- Dasboard/dasboard.py
+++ Dasboard/dasboard.py
@@ -1,44 +1,38 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
 import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-   
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
 # Baca data dari file CSV
 df = pd.read_csv("dataset.csv")
+st.sidebar.title('Jasa Sewa Sepeda JERE')
+
+# side bar
+st.sidebar.image("sepeda.png", caption='ig : sewasepeda_jere', use_column_width=True)
 
 # Judul Dashboard
 st.title('Dashboard Data Persewaan Sepeda')
 
-# Visualisasi distribusi persewaan sepeda berdasarkan musim menggunakan Plotly Express
+# Visualisasi distribusi persewaan sepeda berdasarkan musim 
 st.subheader('Distribusi Persewaan Sepeda berdasarkan Musim')
 fig = px.histogram(df, x='cnt', color='season', 
                    facet_col='season', facet_col_wrap=2,
-                   labels={'cnt': 'Jumlah Sewa Sepeda', 'season': 'Musim'},
-                   title='Distribusi Jumlah Sewa Sepeda Berdasarkan Musim')
+                   labels={'cnt': 'Jumlah Sewa Sepeda', 'season': 'Musim'})
+                   
 st.plotly_chart(fig)
+
 # Keterangan
-keterangan = """
-* Distribusi persewaan sepeda pada musim 1 (Musim Semi) merupakan distribusi right skewness
-* Distribusi persewaan sepeda pada musim 2 (Musim Panas) mendekati distribusi normal
-* Distribusi persewaan sepeda pada musim 3 (Musim Gugur) adalah distribusi left skewness
-* Distribusi persewaan sepeda pada musim 4 (Musim Dingin) mendekati distribusi normal
+keterangan_musim = """
+* Distribusi persewaan sepeda pada musim 1 (Semi) merupakan distribusi right skewness
+* Distribusi persewaan sepeda pada musim 2 (Panas) mendekati distribusi normal
+* Distribusi persewaan sepeda pada musim 3 (Gugur) adalah distribusi left skewness
+* Distribusi persewaan sepeda pada musim 4 (Dingin) mendekati distribusi normal
 """
-st.markdown(keterangan)
 
+st.markdown(keterangan_musim)
 
-
-
-# Perbandingan rata-rata persewaan sepeda antara hari libur dan hari kerja menggunakan Matplotlib
+# Perbandingan rata-rata persewaan sepeda antara hari libur dan hari kerja 
 st.subheader('Perbandingan Rata-rata Persewaan Sepeda Antara Hari Libur dan Hari Kerja')
 avg_rentals = df.groupby('workingday')['cnt'].mean().reset_index()
 fig, ax = plt.subplots(figsize=(16, 8))
@@ -47,11 +41,26 @@ ax.set(xlabel='Hari Kerja', ylabel='Rata-rata Jumlah Sewa Sepeda',
        title='Rata-rata Jumlah Sewa Sepeda Berdasarkan Hari Libur dan Hari Kerja')
 st.pyplot(fig)
 
+
+df = pd.read_csv("dataset.csv")  
+
+# Hitung rata-rata jumlah sewa sepeda per hari pada hari libur
+avg_rentals_weekend = round(df[df['workingday'] == 0]['cnt'].mean(), 0)
+
+# Hitung rata-rata jumlah sewa sepeda per hari pada hari kerja
+avg_rentals_weekday = round(df[df['workingday'] == 1]['cnt'].mean(), 0)
+
+# Tampilkan jumlah rata-rata sewa sepeda per hari pada hari libur
+st.markdown(f"**Rata-rata Sewa Sepeda (Hari Libur):** {avg_rentals_weekend} sepeda/hari")
+
+# Tampilkan jumlah rata-rata sewa sepeda per hari pada hari kerja
+st.markdown(f"**Rata-rata Sewa Sepeda (Hari Kerja):** {avg_rentals_weekday} sepeda/hari")
+
+
 # Keterangan Perbandingan Rata-rata Persewaan Sepeda Antara Hari Libur dan Hari Kerja
 keterangan_hari = """
-* Rata-rata jumlah sepeda yang disewa pada hari libur lebih banyak daripada hari kerja.
+Dari gambar diatas, jumlah orang yang menyewa sepeda di hari kerja cenderung lebih tinggi pada hari libur dibandingkan dengan hari libur. Hal ini mungkin disebabkan oleh minat orang-orang untuk berolahraga saat berangkat kerja, dan pada hari libur orang-orang lebih memilih untuk tetap dirumah..
 """
 
 st.markdown(keterangan_hari)
-
 
